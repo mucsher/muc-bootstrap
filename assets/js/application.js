57c46a88be47636d02d338293ac1f25ea1483d7a
giangnt <giangnt@exoplatform.com>
2013-06-18 16:11:23 +0700

    muc-bootsrtap v.1.0

---
+++ assets/js/application.js
@@ -0,0 +1,31 @@
+// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
+// IT'S ALL JUST JUNK FOR OUR DOCS!
+// ++++++++++++++++++++++++++++++++++++++++++
+
+!function ($) {
+
+  $(function(){
+
+    var $window = $(window)
+
+    // make code pretty
+    window.prettyPrint && prettyPrint()
+
+    // add-ons
+    $('.add-on :checkbox').on('click', function () {
+      var $this = $(this)
+        , method = $this.attr('checked') ? 'addClass' : 'removeClass'
+      $(this).parents('.add-on')[method]('active')
+    })
+
+    // popover demo
+    $("a[rel=popover]")
+      .popover()
+      .click(function(e) {
+        e.preventDefault()
+      })
+
+    // carousel demo
+    $('#myCarousel').carousel()
+
+}(window.jQuery)
---
+++ assets/js/build-js/combile-javascript.bat
@@ -0,0 +1,5 @@
+echo OFF
+cls
+copy /b bootstrap-transition.js+bootstrap-alert.js+bootstrap-modal.js+bootstrap-dropdown.js+bootstrap-scrollspy.js+bootstrap-tab.js+bootstrap-tooltip.js+bootstrap-popover.js+bootstrap-button.js+bootstrap-collapse.js+bootstrap-carousel.js+bootstrap-typeahead.js+bootstrap-affix.js  bootstrap.js
+echo Combile Successful!
+PAUSE
